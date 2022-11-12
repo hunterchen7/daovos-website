1b6668c9bfdd4ee7a017c4f649a8ff4f19137278
Hunter Chen <0x@hunterchen.ca>
2022-11-12 12:47:01 -0500

    ticket

---
+++ components/Ticket.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const Ticket = (props: any): JSX.Element => {
+  const [ticketOpen, setTicketOpen] = useState(false);
+
+  const { title, price, description } = props;
+  return (
+    <div
+      onClick={() => setTicketOpen(!ticketOpen)}
+      className="flex justify-center self-center text-center hover:cursor-pointer"
+    >
+      <div className="w-[90px] h-[130px] md:w-[120px] md:h-[180px] bg-[#E2FF8A] text-2xl md:text-3xl lg:text-4xl font-semibold flex flex-col justify-center text-center border border-black">
+        {price}
+      </div>
+      <div className="w-[120px] md:w-[150px] flex flex-col justify-center">
+        <div className="my-3 h-[110px] md:h-[160px] border-y border-black flex flex-col justify-center text-xl md:text-2xl font-semibold">
+          {title}
+        </div>
+      </div>
+      <div className="w-[100px] h-[130px] md:h-[180px] md:w-[100px] bg-[#E2FF8A] flex flex-col justify-center border-black border">
+        <svg
+          width="87"
+          height="39"
+          viewBox="0 0 87 39"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="mx-auto"
+        >
+          <path
+            d="M26.0348 38.4C40.8188 29.472 71.9228 20.448 86.8988 19.392V19.008C71.6348 17.952 40.2428 8.832 26.0348 0.191998L25.4588 0.959999C27.2828 6.816 31.7948 14.208 35.6348 18.24L0.11475 15.936V22.464L35.6348 20.352C31.8908 24.288 27.4748 31.488 25.4588 37.632L26.0348 38.4Z"
+            fill="black"
+          />
+        </svg>
+      </div>
+      {
+        ticketOpen && 
+        <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white font-nunito p-10 rounded-3xl border border-2 z-[101]'>
+          
+          <h1 className="text-3xl leading-loose">{title}</h1>
+          <div>{description}</div>
+        </div>
+      }
+      <div className={`fixed top-0 left-0 w-full h-full backdrop-blur-sm ${!ticketOpen ? 'hidden' : ''} z-[100]`}></div> {/* backdrop for ticket window */}
+    </div>
+  );
+};
+
+export default Ticket;
--- components/Tickets.tsx
+++ components/Tickets.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import {
   useAddress,
   useDisconnect,
@@ -9,6 +9,7 @@ import {
   useNetworkMismatch,
   ConnectWallet,
 } from '@thirdweb-dev/react';
+import Ticket from './Ticket';
 
 import {
   ChainId,
@@ -18,40 +19,6 @@ import {
 } from '@thirdweb-dev/sdk';
 
 const Tickets = (): JSX.Element => {
-  const Ticket = (props: any): JSX.Element => {
-    const { title, price } = props;
-    return (
-      <div
-        onClick={() => claim({ price })}
-        className="flex justify-center self-center text-center hover:cursor-pointer"
-      >
-        <div className="w-[90px] h-[130px] md:w-[120px] md:h-[180px] bg-[#E2FF8A] text-2xl md:text-3xl lg:text-4xl font-semibold flex flex-col justify-center text-center border border-black">
-          {price}
-        </div>
-        <div className="w-[120px] md:w-[150px] flex flex-col justify-center">
-          <div className="my-3 h-[110px] md:h-[160px] border-y border-black flex flex-col justify-center text-xl md:text-2xl font-semibold">
-            {title}
-          </div>
-        </div>
-        <div className="w-[100px] h-[130px] md:h-[180px] md:w-[100px] bg-[#E2FF8A] flex flex-col justify-center border-black border">
-          <svg
-            width="87"
-            height="39"
-            viewBox="0 0 87 39"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="mx-auto"
-          >
-            <path
-              d="M26.0348 38.4C40.8188 29.472 71.9228 20.448 86.8988 19.392V19.008C71.6348 17.952 40.2428 8.832 26.0348 0.191998L25.4588 0.959999C27.2828 6.816 31.7948 14.208 35.6348 18.24L0.11475 15.936V22.464L35.6348 20.352C31.8908 24.288 27.4748 31.488 25.4588 37.632L26.0348 38.4Z"
-              fill="black"
-            />
-          </svg>
-        </div>
-      </div>
-    );
-  };
-
   const address = useAddress();
   const connectWithMetamask = useMetamask();
   //const disconnectWallet = useDisconnect();
@@ -95,7 +62,7 @@ const Tickets = (): JSX.Element => {
       </div>
       <div className="flex flex-col justify-around gap-8 items-center">
         <div className="text-2xl font-semibold mt-8 md:mt-0">
-          Select the ticket that best suits your needs
+          Click on any ticket to learn more
         </div>
         <div></div>
         <div className="grid grid-cols-1 grid-flow-row items-center md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10 lg:gap-14">
@@ -112,7 +79,7 @@ const Tickets = (): JSX.Element => {
           You want to support the cause beyond the price of an individual ticket. Your name will go on the ‘Wall of Fame’ 
           of founding angels of DAOVOS DAO." />
           <Ticket title="Founding Organisation" price="25,000 USDC" totalAvailable="12"
-          description="" />
+          description="placeholder text" />
           <Ticket title="Partner" price="80,000 USDC" totalAvailable="2"
           description="We’re excited to offer two spaces as full partners on DAOVOS
            inaugural event in 2023. Our partners will have premium full size logo placement,
