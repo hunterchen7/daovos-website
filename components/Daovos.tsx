8a7476baf88cfd8d2765833e6d87b80b492d0d68
Hunter Chen <0x@hunterchen.ca>
2023-01-06 00:36:51 -0500

    fixed spacing issue

--- components/Daovos.tsx
+++ components/Daovos.tsx
@@ -6,9 +6,9 @@ import telegram from '../public/telegram.svg';
 
 const Daovos = (): JSX.Element => {
   return (
-    <div className="flex flex-col md:flex-row justify-around w-full h-screen border-b-2 border-black" id="daovos">
-      <Image src={daovos_icon} alt="daovos_icon" className="md:scale-125 pb-16 md:pb-0"/>
-      <div className="-mt-32 lg:mt-0 flex flex-col gap-3 justify-center">
+    <div className="flex flex-col md:flex-row justify-around w-full h-screen border-b-2 border-black gap-3 my-8" id="daovos">
+      <Image src={daovos_icon} alt="daovos_icon" className="md:scale-125"/>
+      <div className="flex flex-col gap-3 justify-center">
         <h1 className="font-semibold text-8xl">
           DAO Forum
         </h1>
@@ -18,15 +18,15 @@ const Daovos = (): JSX.Element => {
         <h1 className="font-nunito text-4xl font-semibold">
           The DAO Forum
         </h1>
-        <div className="flex gap-3">
+        <div className="flex gap-3 mb-6">
           <a href='https://twitter.com/daovosglobal' target="_blank" rel='noreferrer' className='hover:bg-yellowGreen duration-150 ease-in-out rounded-full'>
-          <Image src={twitter} alt="twitter" className="hover:cursor-pointer" />
+            <Image src={twitter} alt="twitter" className="hover:cursor-pointer" />
           </a>
           {/**<a className='hover:bg-yellowGreen duration-150 ease-in-out rounded-full'>
           <Image src={discord} alt="discord" className="hover:cursor-pointer" />
           </a>**/}
           <a href="https://t.me/+LCxWqCT2H9QxMjA0" target="_blank" rel='noreferrer' className='hover:bg-yellowGreen duration-150 ease-in-out rounded-full'>
-          <Image src={telegram} alt="telegram" className="hover:cursor-pointer" />
+            <Image src={telegram} alt="telegram" className="hover:cursor-pointer" />
           </a>
         </div>
       </div>
