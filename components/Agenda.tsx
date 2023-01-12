c4cde3548f42ff97922e013dd00e9245c563a4c0
DaDo <dohne12@gmail.com>
2023-01-12 15:25:29 +0100

    final agenda

--- components/Agenda.tsx
+++ components/Agenda.tsx
@@ -30,41 +30,59 @@ const Agenda = (): JSX.Element => {
       </div>
       <ul className="list-none text-3xl font-nunito my-8 w-1/2 mx-auto space-y-4">
         <li>
-          <b>1:05 - 1:10 PM</b> Welcome
+          1:00 - 1:15 pm <br/><b>Open doors</b> <br/>
         </li>
         <li>
-          <b>1:10 - 1:30 PM</b> Web3&apos;s climate impact, can it help us get to net zero?
+          1:15 - 1:30 pm <br/><b>Welcome Address and Introductions</b> <br/>
+        </li>
+        <li>
+          1:30 - 2:00 pm <br/><b>Web3&apos;s climate impact: can it help us get to net zero?</b>
+          <p>Speakers from</p>
           <ul className="list-disc text-2xl mx-12">
-            <li>Toucan Protocol</li>
             <li>Foresight Institute</li>
+            <li>Crypto Commons Association</li>
             <li>Hyphen Earth</li>
-            <li>Moderated by Tien</li>
+            <p>Moderated by Tien Ma (Redhill)</p> <br/>
           </ul>
-        </li>
-        <li>
-          <b>1:35 - 1:45 PM</b> (TBD) Spotlight 1
         </li>
         <li>        
-          <b>1:50 - 2:10 PM</b> From theory to practice: governance learnings for the next wave of DAOs
+          2:05 - 2:35 pm <br/><b>From theory to practice: governance learnings for the next wave of DAOs</b>
+          <p>Speakers from</p>
           <ul className="list-disc text-2xl mx-12">
             <li>Maker DAO</li>
-            <li>Palmera DAO</li>
-            <li>Bankless</li>
-            <li>Moderated by Victoria/Fabio</li>
+            <li>Palmera</li>
+            <li>Bankless DAO</li>
+            <li>Anne-Grace Kleczewski</li> <br/>
           </ul>
+          <p>moderated by Victoria (FTW DAO)</p>
         </li>
         <li>
-          <b>2:15 - 2:25 PM</b> (TBD) Spotlight 2
-        </li>
-        <li>
-          <b>2:30 - 3:00 PM</b>
+          2:30 - 3:00 pm <br/><b>Learning Circle: Investing in DAOs</b>
+          <p>Speakers from</p>
           <ul className="list-disc text-2xl mx-12">
-            <li>Learning Circle: Investing in DAOs</li>
-            <li>Fishbowl discussion with input from Tokentus, FTW DAO, Smape Capital, and others</li>
+            <li>Tokentus</li>
+            <li>1kx</li>
+            <li>Smape Capital</li>
+            <li>Fabio Moura</li>
+          </ul>
+          <p>moderated by Mona (Tokentus)</p> <br/>
+        </li>
+        <li>
+          3:10 pm <b>DAO Suisse Introduction</b> <br/>
+        </li>
+        <li>
+          3:20 pm <b>DAOVOZ Community Invitation</b> <br/>
+        </li>
+        <li>
+          3:30 - 3:50 pm <br/><b>Legal Q&A for those with questions on DAOs</b>
+          <p>Speakers</p>
+          <ul className="list-disc text-2xl mx-12">
+            <li>Fabio Moura</li>
+            <li>Anne-Grace Kleczewski</li><br/>
           </ul>
         </li>
         <li>
-          <b>3:00 - 4:00 PM</b> Networking
+          3:40 - 4:30 pm <br/><b>Networking Lounge and drinks</b>
         </li>
       </ul>
     </div>
